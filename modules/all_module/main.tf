69a1e1b6b3af370755540fd061772e864d5b4f63
ssungz789 <nin7344@naver.com>
2024-07-12 17:21:01 +0900

    userdata collection

--- modules/all_module/main.tf
+++ modules/all_module/main.tf
@@ -12,8 +12,8 @@ resource "aws_instance" "web-svr" {
   key_name = var.key_name
   vpc_security_group_ids = [aws_security_group.web-sg.id]
   associate_public_ip_address = true
-  user_data_replace_on_change = true
   subnet_id = var.subnet_id
+  user_data = data.template_file.user_data.rendered
   tags = {
     Name = var.instance_name
   }
